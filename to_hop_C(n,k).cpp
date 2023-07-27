02907ddc2ebdbd081daabab172eb59dbb2aed992
Nauh24 <nvnauh@gmail.com>
2023-07-27 09:30:57 +0700

    1457 - BÀI 8. TỔ HỢP C(n, k)

---
+++ to_hop_C(n,k).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ll mod=1e9+7;
+int n,k;
+ll c[1001][1001];
+void solve(){
+    for(int i=0;i<=1000;i++){
+        for(int j=0;j<=1000;j++){
+            if(j==0||j==i) c[i][j]=1;
+            else if(j==1||j==i-1) c[i][j]=i;
+            else c[i][j]=c[i-1][j-1]+c[i-1][j];
+            c[i][j]%=mod; 
+        }
+    }
+}
+int main(){
+ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  solve();  
+int t;cin>>t;
+while(t--){
+cin>>n>>k;
+cout<<c[n][k];
+    cout<<endl;
+}
+    system("pause");
+}
