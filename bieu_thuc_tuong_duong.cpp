9e85f004497e8b53b9862bf14670d3e0f8a67652
Nauh24 <nvnauh@gmail.com>
2023-08-05 09:23:25 +0700

    1515 - BIỂU THỨC TƯƠNG ĐƯƠNG

---
+++ bieu_thuc_tuong_duong.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main(){
+ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+int t;cin>>t;
+while(t--){
+string s;cin>>s;
+stack<char> st;
+string ans="";
+int ok=0;
+for(int i=0;i<s.size();i++){
+    if(s[i]>='a'&&s[i]<='z') {
+        st.push(s[i]);
+        ans+=st.top();
+    }
+    else if(s[i]=='+'||s[i]=='-'){
+        if(ok==1) {
+            if(s[i]=='+') st.push('-');
+            else st.push('+');
+        }
+        else st.push(s[i]);
+       ans+=st.top(); 
+    }
+    else{
+        if(s[i]=='(') {
+            if(!st.empty()&&st.top()=='-') ok=1;
+            else ok=0;
+        }
+        else ok=0;
+    }
+
+}
+cout<<ans;
+    cout<<endl;
+}
+    system("pause");
+}
