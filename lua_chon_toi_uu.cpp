2a8344fa95b8d9ae93ec36a82298713aa7975748
Nauh24 <nvnauh@gmail.com>
2023-08-21 10:41:18 +0700

    S43 - LỰA CHỌN TỐI ƯU

---
+++ lua_chon_toi_uu.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+bool cmp(pair<ll,ll> a,pair<ll,ll> b){
+    return a.second<b.second;
+}
+int main(){
+int t;cin>>t;
+while(t--){
+int n;cin>>n;
+vector<pair<ll,ll>> v(n);
+for(int i=0;i<n;i++){
+    cin>>v[i].first;
+    cin>>v[i].second;
+}
+sort(v.begin(),v.end(),cmp);
+ll end=v[0].second;
+int cnt=1;
+for(int i=1;i<n;i++){
+    if(v[i].first>=end){
+        end=v[i].second;
+        cnt++;
+    }
+}
+cout<<cnt;
+    cout<<endl;
+}
+    system("pause");
+}
